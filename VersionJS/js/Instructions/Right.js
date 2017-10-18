3667a5665c47892e84a37ec05beda63e46b441b1
DylanBunel <dylan.bunel.49@gmail.com>
2017-10-18 14:37:59 +0200

    setProperty , moveto and deplacement

--- VersionJS/js/Instructions/Right.js
+++ VersionJS/js/Instructions/Right.js
@@ -1,1 +1,20 @@
-class Right extends Instruction
+class Right extends Instruction {
+    constructor(object, x, interval_x) {
+        this.object = object;
+        this.type = "Right";
+        this.x = x;
+        this.interval_x = interval_x;
+    }
+    execute() {
+        if (this.object.getX() < this.x) {
+            
+                this.object.setX(this.object.getX()+this.interval_x);
+            
+                setTimeout(function() {
+            
+                  this.execute();
+            
+                }, FRAME_RATE/60);
+        }
+    }
+}
--- VersionJS/js/Instructions/SetProperty.js
+++ VersionJS/js/Instructions/SetProperty.js
@@ -1,1 +1,36 @@
-class SetProperty extends Instruction
+class SetProperty extends Instruction {
+    constructor(object, property, value) {
+        this.object = object;
+        this.type = "SetProperty";
+        this.value = value;
+        this.property = property;
+    }
+    execute() {
+        switch(property) {
+            case "x":
+                this.object.setX(this.value);
+                break;
+
+            case "y":
+                this.object.setX(this.value);
+                break;
+
+            case "text":
+                this.object.setText(this.value);
+                break;
+
+            case "fgcolor":
+                this.object.set(this.value);
+                break;
+
+            case "bgcolor":
+                this.object.set(this.value);
+                break;
+
+            case "bocolor":
+                this.object.set(this.value);
+                break;
+
+                    }
+    }
+}
