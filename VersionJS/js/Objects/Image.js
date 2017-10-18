3acb1974dbe4295754433becec497df5958c6f5e
PierreGranier <piergranier@laposte.net>
2017-10-18 14:38:22 +0200

    Renommage qui se rapport au fromage

--- VersionJS/js/Objects/Image.js
+++ VersionJS/js/Objects/Image.js
@@ -1,6 +1,6 @@
-class Image extends Object {
-    constructor(id, x, y, image, red, green, blue, state, layer) {
-        super(id, x, y, red, green, blue, state, layer);
+class Image extends AnimatedObject {
+    constructor(id, x, y, image, fgcolor, bgcolor, state, layer) {
+        super(id, x, y, fgcolor, bgcolor, state, layer);
         this.image = image;
     }
 
