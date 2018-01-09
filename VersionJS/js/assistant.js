41cf00cb0406834a1db543ff80d5a88f8be26819
ThibautRoperch <thibaut.roperch@free.fr>
2018-01-09 17:43:27 +0100

    Animation affichée dans l'assistant

--- VersionJS/js/assistant.js
+++ VersionJS/js/assistant.js
@@ -1,28 +1,46 @@
-let last_id = 0;
-let objects_list = document.getElementById("objects");
-let objects_array = new Array();
-let instructions_array = new Array();
+var last_id = -1;
+var objects_list = document.getElementById("objects");
+var objects_array = new Array();
+var instructions_array = new Array();
+
+wait_for_includes();
+function wait_for_includes() {
+	// Check if animation files are included
+	if (!ANIMATION_FILES_INCLUDED) {
+		console.log("Animation files are not included. Include them by this way :\n<script>include_animation_files(\"path/of/AnimationFramework/\");</script>");
+	}
+
+	// Loop with delay until main animation classes are not loaded
+	if (typeof(p5) === "undefined" || typeof(Animation) === "undefined") {
+		setTimeout(function() {
+			wait_for_includes();
+		}, 50);
+	} else {
+		draw_animation();
+	}
+}
 
 function new_object(object_dom) {
-	let obj_id = ++last_id
-	let object;
+	var obj_id = ++last_id;
+	var object;
 
-	let li = document.createElement("li");
+	var li = document.createElement("li");
 		li.id = obj_id;
-	let header = document.createElement("header");
+	var header = document.createElement("headerobj");
 		header.onclick = function() { expand(obj_id); };
-		let id = document.createElement("id");
+		var id = document.createElement("id");
 			id.innerHTML = "Object identifier : " + obj_id;
 			header.appendChild(id);
-		let type = document.createElement("type");
+		var type = document.createElement("type");
 			type.innerHTML = "Object type : " + object_dom.innerHTML;
 			header.appendChild(type);
-		let arrow = document.createElement("arrow");
+		var arrow = document.createElement("arrow");
 			arrow.innerHTML = "\\/";
 			header.appendChild(arrow);
 	li.appendChild(header);
-	let section = document.createElement("section");
-		let article1 = document.createElement("article");
+	var section = document.createElement("sectionobj");
+		// Properties
+		var article1 = document.createElement("article");
 			// x
 			property = document.createElement("property");
 				property.className = "x";
@@ -167,32 +185,77 @@ function new_object(object_dom) {
 					property.appendChild(input);
 				article1.appendChild(property);
 			section.appendChild(article1);
-		let article2 = document.createElement("article");
-			let button = document.createElement("button");
-			article2.appendChild(button);
+		// Instructions
+		var article2 = document.createElement("article");
+			var instructions = document.createElement("instructions");
+				var categories = ["Position", "Move", "Interact", "Instruction", "Change property"];
+				for (cat of categories) {
+					button = document.createElement("button");
+						button.onclick = function() { display_instructions(this); };
+						button.innerHTML = cat;
+						instructions.appendChild(button);
+				}
+				article2.appendChild(instructions);
 			section.appendChild(article2);
-	li.appendChild(section);
+		li.appendChild(section);
 	objects_list.appendChild(li);
 
 	// switch case
-	switch (object_dom.value) {
+	switch (object_dom.innerHTML) {
 		case "Text":
 			property = document.createElement("property");
-					property.className = "text";
-					label = document.createElement("label");
-						label.innerHTML = "text";
-						property.appendChild(label);
-					input = document.createElement("input");
-						input.type = "text";
-						input.onchange = function() { change_property(obj_id, this); };
-						property.appendChild(input);
-					article1.appendChild(property);
-			object = new Text(obj_id, 0, 0, [0, 0, 0], true, [0, 0, 0], true, 0, false, 1, 0); // todo
+				property.className = "text";
+				label = document.createElement("label");
+					label.innerHTML = "text";
+					property.appendChild(label);
+				input = document.createElement("input");
+					input.type = "text";
+					input.onchange = function() { change_property(obj_id, this); };
+					property.appendChild(input);
+				article1.appendChild(property);
+			object = new Text(obj_id, 0, 0, [0, 0, 0], true, [0, 0, 0], true, DEFAULT_STATE, 0, false, 1, 0); // todo
 			break;
 		case "Image":
 			object = new ImageFile();
 			break;
 		case "Rectangle":
+			property = document.createElement("property");
+				property.className = "width";
+				label = document.createElement("label");
+					label.innerHTML = "width";
+					property.appendChild(label);
+				input = document.createElement("input");
+					input.type = "number";
+					input.value = "50";
+					input.required = "required";
+					input.onchange = function() { change_property(obj_id, this); };
+					property.appendChild(input);
+				article1.appendChild(property);
+			property = document.createElement("property");
+				property.className = "height";
+				label = document.createElement("label");
+					label.innerHTML = "height";
+					property.appendChild(label);
+				input = document.createElement("input");
+					input.type = "number";
+					input.value = "50";
+					input.required = "required";
+					input.onchange = function() { change_property(obj_id, this); };
+					property.appendChild(input);
+				article1.appendChild(property);
+			property = document.createElement("property");
+				property.className = "round";
+				label = document.createElement("label");
+					label.innerHTML = "round";
+					property.appendChild(label);
+				input = document.createElement("input");
+					input.type = "text";
+					input.placeholder = "0,0,0,0";
+					input.onchange = function() { change_property(obj_id, this); };
+					property.appendChild(input);
+				article1.appendChild(property);
+			object = new Rectangle(obj_id, 0, 0, [0, 0, 0], true, [0, 0, 0], true, DEFAULT_STATE, 0, false, 1, 0, 50, 50, [0, 0, 0, 0]);
+			console.log(object);
 			break;
 		case "Polygon":
 			break;
@@ -206,8 +269,8 @@ function new_object(object_dom) {
 			break;
 	}
 
-	objects_array[last_id] = object;
-	instructions_array[last_id] = new Array();
+	objects_array[obj_id] = object;
+	instructions_array[obj_id] = new Array();
 }
 
 function new_instruction(object_id, object_dom) {
@@ -225,18 +288,89 @@ function change_property(object_id, property_dom) {
 			objects_array[object_id].setX(property_dom.value);
 			break;
 	}
+
+	draw_animation();
 }
 
 function expand(object_id) {
-	let object_dom = document.getElementById(object_id);
-	object_dom.getElementsByTagName("section")[0].className = "displayed";
-	object_dom.getElementsByTagName("header")[0].onclick = function() { reduce(object_id); };
+	var object_dom = document.getElementById(object_id);
+	object_dom.getElementsByTagName("sectionobj")[0].className = "displayed";
+	object_dom.getElementsByTagName("headerobj")[0].onclick = function() { reduce(object_id); };
 	object_dom.getElementsByTagName("arrow")[0].innerHTML = "/\\";
 }
 
 function reduce(object_id) {
-	let object_dom = document.getElementById(object_id);
-	object_dom.getElementsByTagName("section")[0].className = "hidden";
-	object_dom.getElementsByTagName("header")[0].onclick = function() { expand(object_id); };
+	var object_dom = document.getElementById(object_id);
+	object_dom.getElementsByTagName("sectionobj")[0].className = "hidden";
+	object_dom.getElementsByTagName("headerobj")[0].onclick = function() { expand(object_id); };
 	object_dom.getElementsByTagName("arrow")[0].innerHTML = "\\/";
+}
+
+
+/**********************
+ * P5.js drawing
+ */
+
+var canvas;
+var drawing_dom = document.getElementById("drawing");
+
+// re appeller drawanim quand une propertie est changé
+
+function draw_animation() {
+	var layers = new Set();
+
+	new p5(function(draw_ref) {
+
+		draw_ref.preload = function() { // preload function runs once
+			// // Load the backround image
+			// if (this.bg_image != "") {
+			// 	this.bg_image = draw_ref.loadImage(this.bg_image);
+			// }
+			
+			// // Load animation's images
+			// for (var object_id of this.objects_image) {
+			// 	this.objects.get(object_id).loadImage(drawing);
+			// }
+
+			for (var object of objects_array) {
+				layers.add(object.getLayer());
+			}
+		}
+
+		draw_ref.setup = function() { // setup function waits until preload one is done
+			canvas = draw_ref.createCanvas(400, 200);
+			canvas.parent(drawing_dom);
+		}
+		
+		draw_ref.draw = function() {
+			draw_ref.clear();
+
+			draw_ref.frameRate(60);
+			
+			// Display the background image
+			// if (BG_IMAGE != null) {
+			// 	background(BG_IMAGE);
+			// }
+
+			draw_ref.fill(3);
+			draw_ref.rect(100, 50, 75, 75);
+
+			// Display objects of each layer, if they're set as visible
+			for (var layer of layers) {
+				for (var object of objects_array) {
+					if (object.getLayer() == layer && object.getVisible()) {
+						object.draw(draw_ref);
+					}
+				}
+			}
+		}
+
+		// draw_ref.mouseClicked = function() {
+		// 	// Get the visible objects that are under the cursor position
+		// 	animation_obj.canvasClicked(draw_ref);
+		// 	// Prevent default
+		// 	return false;
+		// }
+
+	});
 }
