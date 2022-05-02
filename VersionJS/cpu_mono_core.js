1976a158c72dd4faac4138cb59f6789de639dc74
Florian Fernandes <florian.fernandes@outlook.com>
2022-05-02 13:49:54 +0200

    add js files for examples

---
+++ VersionJS/cpu_mono_core.js
@@ -0,0 +1,2 @@
+import { load_animation } from './js/AnimationFramework/animation_controller.js';
+load_animation("cpu_mono_core.xml", "cpu_mono_core", 640, 300);
---
+++ VersionJS/cpu_pipeline1.js
@@ -0,0 +1,2 @@
+import { load_animation } from './js/AnimationFramework/animation_controller.js';
+load_animation("cpu_pipeline1.xml", "cpu_pipeline1", 639, 275);
