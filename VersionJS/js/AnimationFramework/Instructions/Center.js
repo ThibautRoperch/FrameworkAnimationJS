9955a920a27b99bc3175ae23b359d1e813103c68
pdelepine <delepine.py@gmail.com>
2022-05-11 16:47:07 +0200

    width de text du center

--- VersionJS/js/AnimationFramework/Instructions/Center.js
+++ VersionJS/js/AnimationFramework/Instructions/Center.js
@@ -1,16 +1,23 @@
 /*
  * This instruction center horizontaly and verticaly the object atached to it
  */
+import { Text } from "../Objects/Text.js";
 import { Instruction } from "./Instruction.js";
 export class Center extends Instruction {
 
-	constructor(object) {
+	constructor (object) {
 		super(object);
 	}
-	
-	execute() {
-		this.object.x = ((WIDTH-this.object.getWidth())/2);
-		this.object.y = ((HEIGHT-this.object.getHeight())/2);  
+
+	execute () {
+		if (this.object instanceof Text) {
+			this.object.x = ((WIDTH - this.object.real_width) / 2);
+			this.object.y = ((HEIGHT - this.object.real_height) / 2);
+		} else {
+			this.object.x = ((WIDTH - this.object.width) / 2);
+			this.object.y = ((HEIGHT - this.object.height) / 2);
+		}
+
 	}
-	
+
 }
--- VersionJS/js/AnimationFramework/Instructions/CenterX.js
+++ VersionJS/js/AnimationFramework/Instructions/CenterX.js
@@ -1,6 +1,7 @@
 /**
  * This instruction center horizontaly the object atached to it
  */
+import { Text } from "../Objects/Text.js";
 import { Instruction } from "./Instruction.js";
 export class CenterX extends Instruction {
 
@@ -9,7 +10,11 @@ export class CenterX extends Instruction {
 	}
 
 	execute() {
-		this.object.x = ((WIDTH-this.object.getWidth())/2);
+		if (this.object instanceof Text) {
+			this.object.x = ((WIDTH - this.object.real_width) / 2);
+		} else {
+			this.object.x = ((WIDTH - this.object.width) / 2);
+		}
 	}
 	
 }
