77a5c593a420c4979b60245e4a8f5c1f96142b8c
Smitaty <mitatysimon49@gmail.com>
2022-05-12 14:26:19 +0200

    Changement du nom des attributs et des getters et setters

--- VersionJS/js/AnimationFramework/Objects/Arrow.js
+++ VersionJS/js/AnimationFramework/Objects/Arrow.js
@@ -5,74 +5,74 @@ import { AnimatedObject } from "./AnimatedObject.js";
 
 export class Arrow extends AnimatedObject {
 
-    width_line;
-    height_line;
-    width_triangle;
-    height_triangle;
-    rotation;
+    _width_line;
+    _height_line;
+    _width_triangle;
+    _height_triangle;
+    _rotation;
 
     constructor(id, x, y, background_color, background_transparent, border_color, border_transparency, border_size, state, layer, visible, opacity, angle, width_line, height_line, width_triangle, height_triangle, rotation){
         super(id, x, y, background_color, background_transparent, border_color, border_transparency, border_size, state, layer, visible, opacity, angle);
-        this.width_line = width_line;
-        this.height_line = height_line;
-        this.width_triangle = width_triangle;
-        this.height_triangle = height_triangle;
-        this.rotation = rotation;
+        this._width_line = width_line;
+        this._height_line = height_line;
+        this._width_triangle = width_triangle;
+        this._height_triangle = height_triangle;
+        this._rotation = rotation;
     }
 
     get width_line() {
-        return this.width_line;
+        return this._width_line;
     }
     set width_line(value) {
-        this.width_line = value;
+        this._width_line = value;
     }
 
     get height_line() {
-        return this.height_line;
+        return this._height_line;
     }
     set height_line(value) {
-        this.height_line = value;
+        this._height_line = value;
     }
 
     get width_triangle() {
-        return this.width_triangle;
+        return this._width_triangle;
     }
     set width_triangle(value) {
-        this.width_triangle = value;
+        this._width_triangle = value;
     }
 
     get height_triangle() {
-        return this.width_triangle;
+        return this._width_triangle;
     }
     set height_triangle(value) {
-        this.width_triangle = value;
+        this._width_triangle = value;
     }
 
     get rotation() {
-        return this.rotation;
+        return this._rotation;
     }
     set rotation(value) {
-        this.rotation = value;
+        this._rotation = value;
     }
 
     draw(drawing) {
         drawing.push();
         super.draw(drawing);
         drawing.translate(this.x, this.y);
-        drawing.rotate(this.rotation);
-        drawing.rect(0, 0, this.width_line, this.height_line);
-        drawing.translate(this.width_line, this.height_line / 2);
-        drawing.triangle(0, -(this.width_triangle / 2), this.height_triangle, 0, 0, this.width_triangle / 2);
+        drawing.rotate(this._rotation);
+        drawing.rect(0, 0, this._width_line, this._height_line);
+        drawing.translate(this._width_line, this._height_line / 2);
+        drawing.triangle(0, -(this._width_triangle / 2), this._height_triangle, 0, 0, this._width_triangle / 2);
         drawing.pop();
     }
     
     isClicked(x, y, drawing) {
         drawing.push();
-        drawing.rotate(this.rotation);
+        drawing.rotate(this._rotation);
         // Clic in rectangle's zone
-        let inRectangleZone = (x >= this.x) && (x <= this.x + this.width_line) && (y >= this.y) && (y <= this.y + this.height_line)
+        let inRectangleZone = (x >= this.x) && (x <= this.x + this._width_line) && (y >= this.y) && (y <= this.y + this._height_line)
         // Clic in triangle's zone
-        let inTriangleZone =  (x <= this.x + this.width_line + this.width_triangle && x >= this.x + this.width_line && y >= (this.y - this.height_triangle / 4) && y <= (this.y + this.height_line + this.height_triangle/4));
+        let inTriangleZone =  (x <= this.x + this._width_line + this._width_triangle && x >= this.x + this._width_line && y >= (this.y - this._height_triangle / 4) && y <= (this.y + this._height_line + this._height_triangle/4));
         drawing.pop();
 		return inRectangleZone || inTriangleZone;
     }
@@ -91,15 +91,15 @@ export class Arrow extends AnimatedObject {
         arrow.setAttribute("visible", this.visible);
         arrow.setAttribute("opacity", this.opacity);
         arrow.setAttribute("angle", this.angle); 
-        arrow.setAttribute("width_line", this.width_line);
-        arrow.setAttribute("height_line", this.height_line);
-        arrow.setAttribute("width_triangle", this.width_triangle);
-        arrow.setAttribute("height_triangle", this.height_triangle);
-        arrow.setAttribute("rotation", this.rotation);
+        arrow.setAttribute("width_line", this._width_line);
+        arrow.setAttribute("height_line", this._height_line);
+        arrow.setAttribute("width_triangle", this._width_triangle);
+        arrow.setAttribute("height_triangle", this._height_triangle);
+        arrow.setAttribute("rotation", this._rotation);
         return arrow;
     }
     
     clone() {
-        return new Arrow(this.id, this.x, this.y, this.background_color, this.background_transparent, this.border_color, this.border_transparency, this.state, this.layer, this.visible, this.opacity, this.angle, this.width_line, this.height_line, this.width_triangle, this.height_triangle, this.rotation);
+        return new Arrow(this.id, this.x, this.y, this.background_color, this.background_transparent, this.border_color, this.border_transparency, this.state, this.layer, this.visible, this.opacity, this.angle, this._width_line, this._height_line, this._width_triangle, this._height_triangle, this._rotation);
     }
 }
